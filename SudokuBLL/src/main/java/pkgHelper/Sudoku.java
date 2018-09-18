59718dd4fa62f96d962172360d9c1d0cecc4ae40
Mingyi Yun <43174523+yunmingyi@users.noreply.github.com>
2018-09-18 08:48:15 -0400

    Update Sudoku.java

--- SudokuBLL/src/main/java/pkgHelper/Sudoku.java
+++ SudokuBLL/src/main/java/pkgHelper/Sudoku.java
@@ -1,8 +1,36 @@
 package pkgHelper;
+import java.util.Arrays;
+import pkgHelper.LatinSquare;
 
 public class Sudoku extends LatinSquare{
 
 	private int iSize;
 	private int iSqrtSize;
+	public Sudoku (){
+		super();
+	}
+	public Sudoku(int[][] latinSquare) {
+		super(latinSquare);
+	}
+	protected int[][] getPuzzle()
+	{
+		return super.getLatinSquare();
+	}
+	protected int[] getRegion(int r) {
+		return null;
+	}
+	protected int[] getRegion(int iCol, int iRow) {
+		return null;
+	}
+	protected boolean isSudoku() {
+		return false;
+	}
+	protected boolean isPartialSudoku() {
+		return false;
+	}
+	protected boolean isValidValue(int iCol, int iRow, int iValue) 
+	{
+		return false;
+	}
 	
 }
